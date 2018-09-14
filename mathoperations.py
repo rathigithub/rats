5956b83d5326a6adc5e5d065d86a7dcad4fc9568
rathigithub <43162355+rathigithub@users.noreply.github.com>
2018-09-14 13:00:01 +0530

    Add files via upload

---
+++ mathoperations.py
@@ -0,0 +1,29 @@
+print("# print my name")
+print("Rathi.J")
+print("# to receive two numbers from the user and add them")
+a=float(input("enter the first number"))
+b=float(input("enter the second number"))
+sum=a+b
+print(sum," is the sum of the two numbers")
+print("# to receive two numbers from the user and subtract them")
+a=float(input("enter the first number"))
+b=float(input("enter the second number"))
+subtraction=a-b
+print(subtraction," is the subtracted value")
+print("# to receive two numbers from the user and divide them")
+a=float(input("enter the first number"))
+b=float(input("enter the second number"))
+division=a/b
+print(division," is the divided value")
+print("# to receive two numbers from the user and multiply them")
+a=float(input("enter the first number"))
+b=float(input("enter the second number"))
+multiplication=a*b
+print(multiplication," is the multiplied value")
+print("# to find the greatest number between two numbers using 'if' conditional statement ")
+a=float(input("enter the first number"))
+b=float(input("enter the second number"))
+if(a>b):
+	print(a," is greater")
+else:
+	print(b," is greater")
