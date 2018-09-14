d9a669ce2931f07c0bfd62c2db45d1d3ec915583
rathigithub <43162355+rathigithub@users.noreply.github.com>
2018-09-14 16:12:10 +0530

    Add files via upload

---
+++ findgrades.py
@@ -0,0 +1,10 @@
+print("# to find your grade ")
+a=float(input("enter your marks"))
+if(a>=85):
+	print("your grade is A. CONGRADULATIONS!!!!!")
+elif(a>=50):
+	print("your grade is B. CONGRADULATIONS!!!!!")
+elif(a>=35):
+	print("your grade is C. TRY HARDER...")
+else:
+	print("your failed. BETTER LUCK NEXT TIME....")
