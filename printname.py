800610123bf3ffb74a8fb02178f4253e57014749
rathigithub <43162355+rathigithub@users.noreply.github.com>
2018-09-17 19:28:51 +0530

    Update printname.py

--- printname.py
+++ printname.py
@@ -1,6 +1,6 @@
 #a program for printing my name
 print ("rathi")
-#write a program to take two numbers from the user
+#write a program to take two numbers from the user and add them.
 a=int(input("enter the value of a"))
 b=int(input("enter the value of b"))
 add=a+b
