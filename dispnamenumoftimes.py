4b695cac5d2f922ff16591af8a6d390eea54d24e
rathigithub <43162355+rathigithub@users.noreply.github.com>
2018-09-18 10:09:08 +0530

    Add files via upload

---
+++ dispnamenumoftimes.py
@@ -0,0 +1,7 @@
+print("# to display 'hello' equivalent to the number of letters in your name. ")
+b=(input("enter your name. "))
+c=len(b)
+n=1
+while(n<=c):
+	print(" hello, ",b," you've got a nice name. :)")
+	n=n+1
